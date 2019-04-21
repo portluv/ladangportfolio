3935d6a15d8ed5fc9ed481419d796ea812423a54
agustinustheo <agustinustheoo@gmail.com>
2019-04-21 14:02:37 +0700

    added config.assets.initialize_on_precompile = false to application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Ladangportfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    
+    # config/application.rb
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
