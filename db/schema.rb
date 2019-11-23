f17cc135f85f9140fea06b28963313be11900806
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2019-11-23 09:54:22 +0700

    Added: added new migration to add new confirmed_profiles column in linkedin_profiles table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191117135553) do
+ActiveRecord::Schema.define(version: 20191118124939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20191117135553) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "confirmed_profiles", default: false
     t.index ["user_id"], name: "index_linkedin_profiles_on_user_id"
   end
 
